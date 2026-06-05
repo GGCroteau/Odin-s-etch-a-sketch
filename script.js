331760952dee0a0388f04e9641a928953aa3e5e3
gguimond <guigc92@hotmail.ca>
2026-06-05 12:15:07 -0400

    -Added the changeGridSize() function that is called on button click. It prompt for a new size, remove all children of ".container" and create a new grid. -Added the mouseover event on squares to turn the color black

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function createGrid(nbSquares){
-    //const size = document.querySelector(".container").clientWidth/nbSquares;
     const size = 100/nbSquares;
-    //adjust .square width / height
+
+    //adjust .square height and row width by modifying the var "--square-size" used in the css
     document.documentElement.style.setProperty("--square-size", `${size}%`);
 
     //create the grid of squares
@@ -11,6 +11,21 @@ function createGrid(nbSquares){
             createSquare(size, j);
         }   
     }
+}
+
+function changeGridSize(){
+    //prompt to get the size
+    let newSize = prompt("Enter the new grid size (anywhere from 1 to 100)")
+    if(newSize<1)
+        newSize = 1;
+    else if(newSize>100)
+        newSize = 100;
+
+    //delete the content of .container
+    document.querySelector(".container").replaceChildren();
+
+    //call createGrid with the gathered size
+    createGrid(newSize);
 }
 
 function createRow(rowNb){
@@ -24,7 +39,14 @@ function createRow(rowNb){
 function createSquare(size, rowNb){
     const square = document.createElement("div");
     square.classList.add("square");
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "black";
+    });
+
     document.querySelector(`.row-${rowNb}`).appendChild(square);
 }
 
 createGrid(16);
+
+const button = document.querySelector("button");
+button.addEventListener("click", changeGridSize);
